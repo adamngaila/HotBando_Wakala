d0bf8a5949bd6d2fcd39cb6603581a503cfd9bc9
adamngaila <adamngaila@gmail.com>
2023-11-14 16:18:22 +0300

    new hostupdate

--- resources/views/layouts/adminpanel.blade.php
+++ resources/views/layouts/adminpanel.blade.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }} upgrade-insecure-requests" http-equiv="content-Security-Policy">
+        <meta name="csrf-token" content="{{ csrf_token() }}" http-equiv="content-Security-Policy">
 
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>{{ config('app.name', 'HotBando|Admin') }}</title>
