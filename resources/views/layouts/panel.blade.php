2f45749fa1f7b51ec8c2a74f744dc3191baa1a6f
Adam Mwangaila <adamngaila@gmail.com>
2024-02-19 22:12:56 +0300

    wakala purchase package

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }} " http-equiv="content-Security-Policy">
-        
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>{{ config('app.name', 'HotBando') }}</title>
     <!-- plugins:css -->
