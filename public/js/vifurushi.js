bd270bbd6b9d4f05286df8f954c06ec0399818d5
Adam Mwangaila <adamngaila@gmail.com>
2024-02-19 21:43:03 +0300

    wakala purchase package

--- public/js/vifurushi.js
+++ public/js/vifurushi.js
@@ -23,9 +23,7 @@ $(document).ready(function(){
             contentType: false,
             cache: false,
             processData: false,
-            headers: {
-              'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            },
+           
             success:function(response)
             {
                 if(response.status == 'good'){
