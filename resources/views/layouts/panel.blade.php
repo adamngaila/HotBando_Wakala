ac603d78c0a81d92006cd8ad9f4530401cefb204
Adam Mwangaila <adamngaila@gmail.com>
2024-02-19 22:10:25 +0300

    wakala purchase package

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -86,7 +86,9 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.0/Chart.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-   
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    
+    <script src="{{ asset('js/vifurushi.js') }}" defer></script>
     <script src="{{ asset('js/sales-ajax.js') }}" defer></script>
     <script src="{{ asset('js/customer.js')  }}"defer ></script>
     <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script>
