5393e95b3f55d4f960ece65e19fbd879e2e9fe55
Adam Mwangaila <adamngaila@gmail.com>
2024-02-20 08:54:45 +0300

    new updates

--- public/js/vifurushi.js
+++ public/js/vifurushi.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
                     alert('Payment process initiated, Transaction Id:'+ response.tcode +"for package of "+ response.package +" MBPS");
                     $("#pesapal_payment").show();
                     document.getElementById('jina').src = response.redirect_url;
-                    console.log(response[3]);
+                    alert(response["redirect_url"]);
                 }
                if(response.status == 'good'){
                     alert(response.status);
