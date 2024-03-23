4499d2a354a64b05813f205d08d756d6154c1bfc
Adam Mwangaila <adamngaila@gmail.com>
2024-03-24 01:20:32 +0300

    new voxha codes

--- public/js/vocha.js
+++ public/js/vocha.js
@@ -36,7 +36,9 @@ $(document).ready(function(){
             method: 'POST',
             data: formData,
             dataType: 'JSON',
-            
+            contentType: false,
+            cache: false,
+            processData: false,
             success: function(response) {
                 var redirectUrl = response.redirect_url;
 
